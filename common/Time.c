5fc7ab7c0ab923884efeec07f75c5a43470f689b
Vinicius Rangel <vinicius.l.rangel@gmail.com>
2018-09-16 14:14:19 -0300

    Time class

--- common/Time.c
+++ common/Time.c
@@ -1,14 +1,36 @@
 #include "Time.h"
 
 struct __Time {
+    uint8_t Hour;
+    uint8_t Minute;
 };
 
-Time Time_new() {
-  Time instance = malloc(sizeof(struct __Time));
-  return instance;
+Time Time_new(uint8_t hour, uint8_t minute) {
+    ASSERT(0 <= hour && hour < 24, "Hour must be between [0,24)");
+    ASSERT(0 <= minute && minute < 24, "Minute must be between [0,60)");
+    Time instance = malloc(sizeof(struct __Time));
+    instance->Hour = hour;
+    instance->Minute = minute;
+    return instance;
+}
+
+SETTER(Time, uint8_t, Hour)
+SETTER(Time, uint8_t, Minute)
+
+int Time_compareTo(Time this, Time other) {
+    if (this->Hour < other->Hour)
+        return -1;
+    if (this->Hour > other->Hour)
+        return 1;
+    if (this->Minute < other->Minute)
+        return -1;
+    if (this->Minute > other->Minute)
+        return 1;
+    return 0;
 }
 
 void Time_delete(Time instance) {
     free(instance);
+
 }
 
--- common/Time.h
+++ common/Time.h
@@ -2,7 +2,19 @@
 
 typedef struct __Time *Time;
 
-Time Time_new();
+/**
+ * Instantiate a new Time
+ * \param hour Hour between [0,24)
+ * \param minute Minute between [0,24)
+ * \see Time
+ */
+Time Time_new(uint8_t hour, uint8_t minute);
+
+HSETTER(Time, uint8_t, Hour)
+HSETTER(Time, uint8_t, Minute)
+
+int Time_compareTo(Time this, Time other);
 
 void Time_delete(Time instance);
+
 
