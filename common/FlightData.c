b67a1d674ee592114f49748834ad9d5d11556b68
Vinicius Rangel <vinicius.l.rangel@gmail.com>
2018-09-16 16:53:32 -0300

    FlightData class
    (also removed trash c file)

--- common/FlightData.c
+++ common/FlightData.c
@@ -1,14 +1,27 @@
 #include "FlightData.h"
 
 struct __FlightData {
+    Time Schedule;
+    Aeroporto Airport;
+    int8_t Runway;
 };
 
-FlightData FlightData_new() {
-  FlightData instance = malloc(sizeof(struct __FlightData));
-  return instance;
+FlightData FlightData_new(Time Schedule, Aeroporto Airport, int8_t Runway) {
+    FlightData instance = malloc(sizeof(struct __FlightData));
+    instance->Schedule = Schedule;
+    instance->Airport = Airport;
+    instance->Runway = Runway;
+    return instance;
 }
+
+GETTER(FlightData, Time, Schedule);
+
+GETTER(FlightData, Aeroporto, Airport);
+
+GETTER(FlightData, int8_t, Runway);
 
 void FlightData_delete(FlightData instance) {
+    Time_delete(instance->Schedule);
+    Aeroporto_delete(instance->Airport);
     free(instance);
 }
-
--- common/FlightData.h
+++ common/FlightData.h
@@ -1,8 +1,10 @@
 #include "../base.h"
+#include "Time.h"
+#include "Aeroporto.h"
 
 typedef struct __FlightData *FlightData;
 
-FlightData FlightData_new();
+FlightData FlightData_new(Time Schedule, Aeroporto Airport, int8_t Runway);
 
 void FlightData_delete(FlightData instance);
 
