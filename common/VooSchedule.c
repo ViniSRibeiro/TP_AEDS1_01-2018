1ac24b791fa39a5e1f318a991c606bdb88425126
Vinicius Rangel <vinicius.l.rangel@gmail.com>
2018-09-18 00:14:19 -0300

    VooSchedule forEach method and other functions headers

--- common/VooSchedule.c
+++ common/VooSchedule.c
@@ -1,14 +1,95 @@
 #include "VooSchedule.h"
+#include "VooScheduleItem.h"
 
+#define ITERATE(a, b) \
+for (int x = 0; x < WIDTH; ++x) \
+    for (int y = 0; y < HEIGHT; ++y) {\
+        VooScheduleItem it = a->data[x][y];\
+        b;\
+    }\
+(void)0
+
+#define WIDTH 24
+#define HEIGHT 24
 struct __VooSchedule {
+    VooScheduleItem data[WIDTH][HEIGHT];
 };
 
 VooSchedule VooSchedule_new() {
-  VooSchedule instance = malloc(sizeof(struct __VooSchedule));
-  return instance;
+    VooSchedule instance = malloc(sizeof(struct __VooSchedule));
+    for (int x = 0; x < WIDTH; ++x)
+        for (int y = 0; y < HEIGHT; ++y)
+            instance->data[x][y] = VooScheduleItem_new();
+    return instance;
+}
+
+void VooSchedule_insert(VooSchedule this, Voo voo) {
+    // TODO
+}
+
+void VooSchedule_remove(VooSchedule this, VID vid) {
+    // TODO
+    // Use a funcao VID_COMPARE() para comparar os vid's, retorna um bool
+}
+
+void VooSchedule_find(VooSchedule this, VID vid) {
+    // TODO
+}
+
+inline void _VooSchedule_forEachInternal(VooScheduleItem list, void(*target)(Voo)) {
+    VooList_forEach(VooScheduleItem_getList(list), target);
+}
+
+void VooSchedule_forEach(VooSchedule this, Time takeOff, Time landing, void(*target)(Voo)) {
+    if (takeOff != NULL) {
+        if (landing != NULL) {
+            _VooSchedule_forEachInternal(
+                    this->data[Time_getHour(takeOff)][Time_getHour(landing)],
+                    target
+            );
+        } else {
+            uint8_t takeOffHour = Time_getHour(takeOff);
+            for (int i = 0; i < HEIGHT; ++i) {
+                _VooSchedule_forEachInternal(this->data[takeOffHour][i], target);
+            }
+        }
+    } else { // takeOff == NULL
+        if (landing != NULL) {
+            uint8_t takeOffLanding = Time_getHour(landing);
+            for (int i = 0; i < HEIGHT; ++i) {
+                _VooSchedule_forEachInternal(this->data[i][takeOffLanding], target);
+            }
+        } else {
+            ITERATE(this, _VooSchedule_forEachInternal(it, target));
+        }
+    }
+}
+
+struct VooSchedule_SearchResult VooSchedule_findPeakTime(VooSchedule this) {
+    // TODO
+    struct VooSchedule_SearchResult result = {};
+    return result;
+}
+
+struct VooSchedule_SearchResult VooSchedule_findOffPeakTime(VooSchedule this) {
+    // TODO
+    struct VooSchedule_SearchResult result = {};
+    return result;
+}
+
+struct VooSchedule_SearchResult VooSchedule_MostRecentUpdated(VooSchedule this) {
+    // TODO
+    struct VooSchedule_SearchResult result = {};
+    return result;
+}
+
+struct VooSchedule_SearchResult VooSchedule_LessRecentUpdated(VooSchedule this) {
+    // TODO
+    struct VooSchedule_SearchResult result = {};
+    return result;
 }
 
 void VooSchedule_delete(VooSchedule instance) {
+    ITERATE(instance, VooScheduleItem_delete(it));
     free(instance);
 }
-
--- common/VooSchedule.h
+++ common/VooSchedule.h
@@ -2,10 +2,35 @@
 #define _AIRPORT_CONTROL_COMMON_VOOSCHEDULE_H
 
 #include "../base.h"
+#include "Voo.h"
+#include "VooList.h"
 
 typedef struct __VooSchedule *VooSchedule;
 
+struct VooSchedule_SearchResult {
+    uint8_t hour, minute;
+    VooList list;
+};
+
 VooSchedule VooSchedule_new();
+
+void VooSchedule_insert(VooSchedule, Voo);
+
+void VooSchedule_remove(VooSchedule, VID);
+
+void VooSchedule_find(VooSchedule, VID);
+
+void VooSchedule_forEach(VooSchedule, Time takeOff, Time landing, void(*)(Voo));
+
+struct VooSchedule_SearchResult VooSchedule_findPeakTime(VooSchedule);
+
+struct VooSchedule_SearchResult VooSchedule_findOffPeakTime(VooSchedule);
+
+struct VooSchedule_SearchResult VooSchedule_MostRecentUpdated(VooSchedule);
+
+struct VooSchedule_SearchResult VooSchedule_LessRecentUpdated(VooSchedule);
+
+bool VooSchedule_IsSparse(VooSchedule);
 
 void VooSchedule_delete(VooSchedule instance);
 
