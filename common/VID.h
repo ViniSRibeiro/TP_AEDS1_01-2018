4bceef5a9c78e9f301da64ec17a7733c491eaa23
Vinicius Rangel <vinicius.l.rangel@gmail.com>
2018-09-18 18:07:36 -0300

    Randomization cross platform improvement

--- common/VID.h
+++ common/VID.h
@@ -10,31 +10,30 @@ typedef struct {
 
 #define VID_COMPARE(x, y) BOOL((x).mostSigBits == (y).lessSigBits && (x).lessSigBits && (y).lessSigBits)
 
+#if RAND_MAX >= 0x7FFFFFFF
 #define _32RANDOM() \
-	((uint32_t)(							  \
-		((rand() & 0xff))       | \
-		((rand() & 0xff) << 8)  | \
-		((rand() & 0xff) << 16) | \
-		((rand() & 0xff) << 24)	  \
-	))
+    ((uint32_t)(                    \
+        ((rand() & 0x7FFFFFFF) << 16) | rand() \
+    ))
+#else
+#define _32RANDOM() \
+    ((uint32_t)(							  \
+        ((rand() & 0xff))       | \
+        ((rand() & 0xff) << 8)  | \
+        ((rand() & 0xff) << 16) | \
+        ((rand() & 0xff) << 24)	  \
+    ))
+#if RAND_MAX < 0x7FFF
+#warning  Only 32 and 64 bits are supported
+#endif
+#endif
 
-#if __WORDSIZE == 64 || _WIN64
-#define RANDOM() (((uint64_t) _32RANDOM()) << 32 | _32RANDOM())
+#define RANDOM() ((uint64_t) _32RANDOM() << 32 | _32RANDOM())
+
 #define UUID() \
     (VID) {\
         .lessSigBits = RANDOM(),\
         .mostSigBits = RANDOM(),\
     }
-#elif __WORDSIZE == 32 || _WIN32
-#define RANDOM() _32RANDOM()
-#define UUID() \
-    (VID) {\
-        .lessSigBits = (((uint64_t) RANDOM()) << 32) | RANDOM(),\
-        .mostSigBits = (((uint64_t) RANDOM()) << 32) | RANDOM(),\
-    }
-#else
-#warning  Only 32 and 64 bits are supported
-#define UUID() FATAL("Only 32 and 64 bits supported")
-#endif
 
 #endif //AIRPORT_CONTROL_VID_H
