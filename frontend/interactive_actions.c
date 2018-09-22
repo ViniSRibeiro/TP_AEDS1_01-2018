a8025e252386f99b2c019bd4438a8605ed14d4d6
viniciuslrangel <vinicius.l.rangel@gmail.com>
2018-09-22 20:33:40 -0300

    Base name for interactive options

--- frontend/interactive_actions.c
+++ frontend/interactive_actions.c
@@ -7,13 +7,18 @@ static void PRINT3(VooSchedule nothing) {
     PRINTLN("TEST STR");
     PRINTLN("TEST STR");
 }
-
+#define NOP PRINT3
 struct Action actions[] = {
         {"Test", PRINT3},
-        /*{"Inserir voo", ...},
-        {"Remover voo", ...},
-        {"Procurar voo", ...},
-        {"Listar voos", ...},*/
+        {"Inserir voo", NOP},
+        {"Remover voo", NOP},
+        {"Procurar voo", NOP},
+        {"Listar voos", NOP},
+        {"Horario de maior movimentacao", NOP},
+        {"Horario de menor movimentacao", NOP},
+        {"Lista recentemente alterada", NOP},
+        {"Lista alterada a mais tempo", NOP},
+        {"Matriz esparca?", NOP},
 };
 
 int numOfActions() {
