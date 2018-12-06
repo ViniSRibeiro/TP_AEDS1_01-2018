54cbf82fe94b00e071e3995ebd7a983e0cc3368e
ViniSRibeiro <37838123+ViniSRibeiro@users.noreply.github.com>
2018-12-06 01:18:22 -0200

    Adição da função de ordenação

--- common/VooScheduleContainer.c
+++ common/VooScheduleContainer.c
@@ -2,23 +2,26 @@
 // Created by vinir on 05/12/2018.
 //
 
+#include <windef.h>
 #include "VooScheduleContainer.h"
 
 
 static struct VSContainer_SortStats sort_bubbleSort
-    (VooSchedule *list, int length, VSContainer_Comparator comparator);
+        (VooSchedule *list, int length, VSContainer_Comparator comparator);
+
 static struct VSContainer_SortStats sort_selectionSort
-    (VooSchedule *list, int length, VSContainer_Comparator comparator);
+        (VooSchedule *list, int length, VSContainer_Comparator comparator);
+
 static struct VSContainer_SortStats sort_insertionSort
-    (VooSchedule *list, int length, VSContainer_Comparator comparator);
+        (VooSchedule *list, int length, VSContainer_Comparator comparator);
+
 static struct VSContainer_SortStats sort_shellSort
-    (VooSchedule *list, int length, VSContainer_Comparator comparator);
+        (VooSchedule *list, int length, VSContainer_Comparator comparator);
 
 struct __VSContainer {
     VooSchedule *list;
     int len;
     int cap;
-    struct VSContainer_SortStats stats; 
 };
 
 VSContainer VSContainer_new() {
@@ -26,27 +29,23 @@ VSContainer VSContainer_new() {
     instance->list = NULL;
     instance->len = 0;
     instance->cap = 0;
-    instance->stats = (struct VSContainer_SortStats){
-        .comps   = 0,
-        .moves   = 0,
-        .elapsed = 0
-    };
     return instance;
 }
 
 void VSContainer_insert(VSContainer this, VooSchedule schedule) {
-    if(this->len + 1 >= this->cap) { // Increase
-        this->cap = this->cap >= 3 ? (int)(this->cap * 1.5f) : this->cap + 1;
+    if (this->len + 1 >= this->cap) { // Increase
+        this->cap = this->cap >= 3 ? (int) (this->cap * 1.5f) : this->cap + 1;
         this->list = realloc(this->list, this->cap);
-        if(!this->list) {
+        if (!this->list) {
             FATAL("Out of memory");
         }
     }
     this->list[this->len++] = schedule;
 }
 
-struct VSContainer_SortStats VSContainer_sort(VSContainer this, enum VSContainer_SortType type, VSContainer_Comparator comparator) {
-    switch(type) {
+struct VSContainer_SortStats
+VSContainer_sort(VSContainer this, enum VSContainer_SortType type, VSContainer_Comparator comparator) {
+    switch (type) {
         case VSCONTAINER_BUBBLE:
             return sort_bubbleSort(this->list, this->len, comparator);
         case VSCONTAINER_SELECTION:
@@ -65,74 +64,107 @@ struct VSContainer_SortStats VSContainer_sort(VSContainer this, enum VSContainer
 }
 
 struct VSContainer_SortStats sort_bubbleSort
-    (VooSchedule *list, int length, VSContainer_Comparator comparator) {
-    for (int i = 0; i < length - 1; i++){
-        for (int j = (i+1); j < length; j++){
-            if (comparator(list[j], list[i]) < 0){
+        (VooSchedule *list, int length, VSContainer_Comparator comparator) {
+    for (int i = 0; i < length - 1; i++) {
+        for (int j = (i + 1); j < length; j++) {
+            if (comparator(list[j], list[i]) > 0) {
                 VooSchedule temp = list[i];
                 list[i] = list[j];
                 list[j] = temp;
             }
         }
     }
+    return (struct VSContainer_SortStats) {};
 }
 
 struct VSContainer_SortStats sort_selectionSort
-    (VooSchedule *list, int length, VSContainer_Comparator comparator) {i
+        (VooSchedule *list, int length, VSContainer_Comparator comparator) {
 
-    int i, j, min, aux;
-    for (i = 0; i < (tam-1); i++)
-    {
+    int j, min;
+    VooSchedule aux;
+    for (int i = 0; i < (length - 1); i++) {
         min = i;
-        for (j = (i+1); j < tam; j++) {
-            if(num[j] < num[min])
+        for (j = (i + 1); j < length; j++) {
+            if (list[j] < list[min])
                 min = j;
         }
-        if (num[i] != num[min]) {
-            aux = num[i];
-            num[i] = num[min];
-            num[min] = aux;
+        if (comparator(list[i], list[min]) != 0) {
+            aux = list[i];
+            list[i] = list[min];
+            list[min] = aux;
         }
     }
+    return (struct VSContainer_SortStats) {};
 }
 
 struct VSContainer_SortStats sort_insertionSort
-    (VooSchedule *list, int length, VSContainer_Comparator comparator) {
+        (VooSchedule *list, int length, VSContainer_Comparator comparator) {
 
-    int escolhido, j;
+    int j;
+    VooSchedule escolhido;
 
-    for (int i = 1; i < TAM; i++) {
-        escolhido = vetor[i];
+    for (int i = 1; i < length; i++) {
+        escolhido = list[i];
         j = i - 1;
 
-        while ((j >= 0) && (vetor[j] > escolhido)) {
-            vetor[j + 1] = vetor[j];
+        while ((j >= 0) && comparator(list[j], escolhido) < 0) {
+            list[j + 1] = list[j];
             j--;
         }
 
-        vetor[j + 1] = escolhido;
+        list[j + 1] = escolhido;
     }
+    return (struct VSContainer_SortStats) {};
 }
 
 struct VSContainer_SortStats sort_shellSort
-    (VooSchedule *list, int length, VSContainer_Comparator comparator) {
+        (VooSchedule *list, int length, VSContainer_Comparator comparator) {
 
-    int i , j , value;
+    int i, j;
+    VooSchedule value;
     int gap = 1;
-    while(gap < TAM) {
-        gap = 3*gap+1;
+    while (gap > length) {
+        gap = 3 * gap + 1;
     }
-    while ( gap > 1) {
+    while (gap > 1) {
         gap /= 3;
-        for(i = gap; i < TAM; i++) {
-            value = vet[i];
+        for (i = gap; i < length; i++) {
+            value = list[i];
             j = i;
-            while (j >= gap && value < vet[j - gap]) {
-                vet[j] = vet [j - gap];
+            while (j >= gap && comparator(value, list[j - gap]) < 0) {
+                list[j] = list[j - gap];
                 j = j - gap;
             }
-            vet [j] = value;
+            list[j] = value;
         }
     }
+    return (struct VSContainer_SortStats) {};
+}
+
+void Particao(int Esq, int Dir,int *i, int *j, VooSchedule *A){
+    VooSchedule pivo, aux;
+    *i = Esq; *j = Dir;
+    pivo = A[(*i + *j)/2];
+
+    do
+    {
+        while(pivo -> Chave > A[*i] -> Chave) (*i)++;
+        while(pivo -> Chave < A[*j] -> Chave) (*j)--;
+        if(*i <= *j){
+            aux = A[*i]; A[*i] = A[*j]; A[*j] = aux;
+            (*i)++; (*j)--;
+        }
+    }while(*i <= *j);
+}
+
+void Ordena(int Esq, int Dir, VooSchedule *A) {
+    int i, j;
+    Particao(Esq, Dir, &i, &j, A);
+    if (Esq < j)Ordena(Esq, j, A);
+    if (i < Dir)Ordena(i, Dir, A);
+}
+
+void QuickSort(VooSchedule *A, int n) {
+    Ordena(0, n - 1, A);
 }
 
