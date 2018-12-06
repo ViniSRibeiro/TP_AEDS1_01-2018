bf28383d4cc869096066d2df2ea018911766e55a
ViniSRibeiro <37838123+ViniSRibeiro@users.noreply.github.com>
2018-12-06 02:11:37 -0200

    Add HeapSort

--- common/VooScheduleContainer.c
+++ common/VooScheduleContainer.c
@@ -168,3 +168,31 @@ void sort_QuickSort_main(VooSchedule *A, int n,VSContainer_Comparator comparator
     sort_QuickSort_Ordena(0, n - 1, A,comparator);
 }
 
+void sort_heapsort(VooSchedule a[], int n) {
+    int i = n / 2, pai, filho, t;
+    while(true) {
+        if (i > 0) {
+            i--;
+            t = a[i];
+        } else {
+            n--;
+            if (n == 0) return;
+            t = a[n];
+            a[n] = a[0];
+        }
+        pai = i;
+        filho = i * 2 + 1;
+        while (filho < n) {
+            if ((filho + 1 < n)  &&  (a[filho + 1] > a[filho]))
+                filho++;
+            if (a[filho] > t) {
+                a[pai] = a[filho];
+                pai = filho;
+                filho = pai * 2 + 1;
+            } else {
+                break;
+            }
+        }
+        a[pai] = t;
+    }
+}
